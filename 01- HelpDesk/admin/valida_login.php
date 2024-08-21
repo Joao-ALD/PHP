a5b74a98c74c9b8a8f2aeda9565f6130a6365467
Joao <joaovictorald2@gmail.com>
2024-08-21 14:14:56 -0300

    projeto finalizado

--- 01- HelpDesk/admin/valida_login.php
+++ 01- HelpDesk/admin/valida_login.php
@@ -21,17 +21,25 @@
 
     $usuario_autenticado = false;
     $usuario_id = null;
+    $usuario_perfil_id = null;
+
+
+    $perfis = array(
+        1 => 'administrativo',
+        2 => 'usuario'
+    );
 
     $usuarios_app = array(
-        array('id'=> 1, 'email' => 'adm@teste.com.br', 'senha' => '1234'),
-        array('id'=> 2,'email' => 'user@teste.com.br', 'senha' => 'abcd'),
-        array('id'=> 3,'email' => 'user@user.com.br', 'senha' => '1234')
+        array('id'=> 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1), // => significa 'Recebe'
+        array('id'=> 2, 'email' => 'user@teste.com.br', 'senha' => 'abcd', 'perfil_id' => 1),
+        array('id'=> 3, 'email' => 'user@user.com.br', 'senha' => '1234', 'perfil_id' => 2)
     );
 
     foreach($usuarios_app as $user){
         if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
             $usuario_autenticado = true;
             $usuario_id = $user ['id'];
+            $usuario_perfil_id = $user ['perfil_id'];
         };
     };
 
@@ -39,6 +47,7 @@
         //echo 'Usuario autenticado com Sucesso!';
         $_SESSION['autenticado'] = 'SIM';
         $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil_id;
         header('Location:home.php');
     }
     else {
