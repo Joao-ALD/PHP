eeff3d5959a7c0df018a2db648c3806da8a89604
Joao <joaovictorald2@gmail.com>
2024-08-22 15:27:05 -0300

    ,.

--- 02 - Restaurante/admin/opCardapio.php
+++ 02 - Restaurante/admin/opCardapio.php
@@ -2,8 +2,22 @@
 
     require_once "config.php";
 
-    $cardapio = $_POST['txt_cardapio'];
+    // se o txt_cardapio enviado pelo método post não estiver vazio
+    if(!empty($_POST['txt_cardapio'])) { 
+
+        $cardapio = $_POST ['txt_cardapio'];
+        $foto = $_FILES['file_foto']['name'];
+        $foto = str_replace(" ", " ", $foto); // o espaço não é nescessário para criar strings vazias, mas é utilizado para facilitar a visualização
+
+        //Caminho Temporário
+        $foto_temp = $_FILES['file_foto']['tmp_name'];  
+        $destino = "img/" . $foto;
+
+        move_uploaded_file($foto_temp, $destino);
+    };
+
+    $cardapio = $_POST['txt_cardapio'];  
     $foto = $_FILES['file_foto']['name'];
 
-    echo "Cardapio: ". $cardapio ."<br> Foto:" . $foto
+    echo "Cardapio: ". $cardapio ."<br> Foto:" . $foto;
 ?>
--- php-ensinando_require/home.php
+++ php-ensinando_require/home.php
@@ -1,7 +1,7 @@
 <?php
-    // include("menu.php") //caso tente acessar algo inexistente devolve warning, mas ainda permitindo o carregamento do site 
+    // include("menu.php") // include - caso tente acessar algo inexistente devolve warning, mas ainda permitindo o carregamento do site 
 
-    require "menu2.php"; // caso tente acessar algo inexistente devolve fatal error, impedindo o carregamento
+    require "menu2.php"; // require - caso tente acessar algo inexistente devolve fatal error, impedindo o carregamento
     require_once "menu2.php"; //impede a repetição
 ?>
 
