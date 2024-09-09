974274a03e874f88f0518f074f4cd951d2ca71fc
Joao <joaovictorald2@gmail.com>
2024-09-09 16:50:01 -0300

    aula 09/09

--- 03-MVC-Base/config.php
+++ 03-MVC-Base/config.php
@@ -1,16 +1,32 @@
 <?php
-    $config = array(
-        'dbename' => 'mvc',
-        'host'=> 'localhost',
-        'user' => 'root',
-        'dbpass' => ''
-    );
+
+    require 'environment.php';
+
+
+
+    $config = array();
+
+        if(ENVIRONMENT == 'development'){
+            define("Base_URL", "http://localhost//PHP/03-MVC-Base");            
+            $config['dbname'] = 'mvc';
+            $config['host'] = 'localhost';
+            $config['dbuser'] = 'root';
+            $config['dbpass'] = '';
+        }
+        else {
+            define("Base_URL", "http://localondeEstaSendoHospedado.com.br/");
+            $config['dbname'] = 'mvc';
+            $config['host'] = 'localhost';
+            $config['dbuser'] = 'root';
+            $config['dbpass'] = '';
+        };
+    global $db;
 
     try {
-        $db = new PDO("mysql:dbname=".$config['dbname']."host".$config['host'], $config['dbuser'], $config['dbpass']);
+        $db = new PDO("mysql:dbname=".$config['dbname'].";host".$config['host'], $config['dbuser'], $config['dbpass']);
     } 
     catch(PDOException $e){
         echo 'ERRO'.$e -> getMessage();
         exit;
-    }
+    };
 ?>
