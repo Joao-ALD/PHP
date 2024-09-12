9a9bdb71925128857ea2e1ab2fe0ac69a05f782e
Joao <joaovictorald2@gmail.com>
2024-09-12 16:24:48 -0300

    ...

---
+++ 03-MVC-Base/core/controller.php
@@ -0,0 +1,18 @@
+<?php
+    class controller{
+        //responsável por mostrar o que estiver na pasta view
+        public function loadView($viewName, $viewData = array()){
+            extract($viewData); //extract cria um nv arquivo no diretório
+            require 'views'.$viewName .'.php';
+        }
+
+        //trazer os arquivos da view (menu.php)
+        public function loadTemplate($viewName, $viewData = array()){
+            require 'views/template.php';
+        }
+
+        public function loadViewInTemplate($viewName, $viewData = array()){
+            extract($viewData);
+            require 'views' . $viewName.'.php';
+        }
+    }
