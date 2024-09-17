cd216ae6faeac648c04835a2751961e2f561c4d5
Joao <joaovictorald2@gmail.com>
2024-09-17 14:22:14 -0300

    x

---
+++ 04-crud_mvc/config.php
@@ -0,0 +1,29 @@
+<?php
+    require 'environment.php';
+
+    $config = array();
+
+        if(ENVIRONMENT == 'development'){
+            define("Base_URL", "http://localhost//PHP/03-MVC-Base");            
+            $config['dbname'] = 'mvc';
+            $config['host'] = 'localhost';
+            $config['dbuser'] = 'root';
+            $config['dbpass'] = '';
+        }
+        else {
+            define("Base_URL", "http://localondeEstaSendoHospedado.com.br/");
+            $config['dbname'] = 'mvc';
+            $config['host'] = 'localhost';
+            $config['dbuser'] = 'root';
+            $config['dbpass'] = '';
+        };
+    global $db;
+
+    try {
+        $db = new PDO("mysql:dbname=".$config['dbname'].";host".$config['host'], $config['dbuser'], $config['dbpass']);
+    }
+    catch(PDOException $e){
+        echo 'ERRO'.$e -> getMessage();
+        exit;
+    };
+?>
---
+++ 04-crud_mvc/core/Core.php
@@ -0,0 +1,53 @@
+<?php
+    class Core{
+        public function run(){
+            // echo 'URL:' . $_GET['url'];
+
+            //meusite.com.br/contatos/cadastrar/1 (neste ex. : dominio/controller/ação/parametro)
+            // controller faz o redircionamento 
+
+            $url ='/';
+
+            if (isset($_GET['url'])) {
+                // $url = $url . $_GET['url'];
+                $url .= $_GET['url'];
+            }
+
+            // echo "URL: " . $url;
+
+            $params = array();
+
+            if (!empty($url) && $url != '/') { // verifica se foi enviado
+                $url = explode('/', $url); //divide o array
+                array_shift($url); //remove a "primeira" /
+
+                $currentController = $url[0].'Controller';
+                //homeController, qq coisa que vier Controller
+                array_shift($url);
+
+                if (isset($url[0]) && !empty($url[0])) {
+                    $currentAction = $url[0];
+                    array_shift($url);
+                }else{
+                    $currentAction = 'index';
+                }
+
+                if (count($url) > 0) {
+                    $params = $url;
+                }
+            } 
+            else{
+                $currentController = 'homeController';
+                $currentAction = 'index';
+            }
+
+            // echo 'CONTROLLER: ' . $currentController . '<br>';
+            // echo 'ACTION: ' . $currentAction . '<br>';
+            // echo 'PARAMS: ' . print_r($params, true) . '<br>';
+
+            $c = new $currentController(); //new - prepara para receber em qualquer outra parte do programa 
+
+            call_user_func_array(array($c, $currentAction), $params);
+        }
+    }
+?>
